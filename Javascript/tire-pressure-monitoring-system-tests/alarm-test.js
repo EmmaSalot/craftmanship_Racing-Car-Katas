2c48b9d6f8e05fef2cf52922eb2ec45424ae2ac7
Emma Salot <emma.salot.etu@univ-lille.fr>
2024-06-12 18:18:58 +0200

    exo1(tire-pressure-monitoring) : refactor and add new tests for alarm

--- Javascript/tire-pressure-monitoring-system-tests/alarm-test.js
+++ Javascript/tire-pressure-monitoring-system-tests/alarm-test.js
@@ -1,18 +1,46 @@
-/* globals describe, it */
-var chai = require('chai');
-chai.should();
+const chai = require('chai');
+const sinon = require('sinon');
+const Alarm = require('../tire-pressure-monitoring-system/alarm.js');
 
-var Alarm = require('../tire-pressure-monitoring-system/alarm.js');
+chai.should();
 
 describe('Tyre Pressure Monitoring System', function() {
 
-	describe('Alarm', function() {
+    describe('Alarm', function() {
 
-		it('foo', function() {
-			var alarm = new Alarm();
-			alarm.alarmOn().should.equal(false);
-		});
+        it('should not trigger alarm when pressure is within range', function() {
+            const sensorStub = sinon.stub();
+            sensorStub.popNextPressurePsiValue = sinon.stub().returns(18);
 
-	});
+            const alarm = new Alarm(sensorStub);
+
+            alarm.check();
+
+            alarm.alarmOn().should.equal(false);
+        });
+
+        it('should trigger alarm when pressure is below threshold', function() {
+            const sensorStub = sinon.stub();
+            sensorStub.popNextPressurePsiValue = sinon.stub().returns(15);
+
+            const alarm = new Alarm(sensorStub);
+
+            alarm.check();
+
+            alarm.alarmOn().should.equal(true);
+        });
+
+        it('should trigger alarm when pressure is above threshold', function() {
+            const sensorStub = sinon.stub();
+            sensorStub.popNextPressurePsiValue = sinon.stub().returns(22);
+
+            const alarm = new Alarm(sensorStub);
+
+            alarm.check();
+
+            alarm.alarmOn().should.equal(true);
+        });
+
+    });
 
 });
