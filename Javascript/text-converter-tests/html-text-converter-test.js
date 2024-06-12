648fe6bcc2df5d342ed2faefd979d81e96ab7c21
Emma Salot <emma.salot.etu@univ-lille.fr>
2024-06-12 18:37:40 +0200

    exo2(text-converter) : add test for html text converter

--- Javascript/text-converter-tests/html-text-converter-test.js
+++ Javascript/text-converter-tests/html-text-converter-test.js
@@ -1,18 +1,37 @@
-/* globals describe, it */
-var chai = require('chai');
-chai.should();
+const chai = require('chai');
+const fs = require('fs');
+const sinon = require('sinon');
+const HtmlTextConverter = require('../text-converter/html-text-converter.js');
 
-var HtmlTextConverter = require('../text-converter/html-text-converter.js');
+chai.should();
 
 describe('Html Converter', function() {
 
-	describe('HtmlTextConverter', function() {
+    describe('HtmlTextConverter', function() {
 
-		it('foo', function() {
-			var converter = new HtmlTextConverter('foo');
-			converter.getFilename().should.equal('fixme');
-		});
+        it('should return correct filename', function() {
+            const filename = 'foo';
+            const converter = new HtmlTextConverter(filename);
 
-	});
+            const result = converter.getFilename();
+
+            result.should.equal(filename);
+        });
+
+        it('should convert plain text to HTML', function() {
+            const filename = 'test.txt';
+            const text = 'This is a test text.\nIt has multiple lines.\n';
+            sinon.stub(fs, 'readFileSync').returns(text);
+            const expectedHtml = 'This is a test text.<br />It has multiple lines.<br />';
+            const converter = new HtmlTextConverter(filename);
+
+            const result = converter.convertToHtml();
+
+            result.should.equal(expectedHtml);
+
+            fs.readFileSync.restore();
+        });
+
+    });
 
 });
